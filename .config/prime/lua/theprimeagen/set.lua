b03ea77df46d7da37c8d95c249469779ef7421af
김찬희 <blackeagle10@icloud.com>
2025-02-17 08:41:49 +0900

    Fix prime set.lua

--- .config/prime/lua/theprimeagen/set.lua
+++ .config/prime/lua/theprimeagen/set.lua
@@ -1,34 +1,31 @@
-local o = vim.opt
+vim.opt.guicursor = ""
 
-o.guicursor = ""
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-o.number = true
-o.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
-local indent = 2
-o.tabstop = indent
-o.softtabstop = indent
-o.shiftwidth = indent
-o.expandtab = true
+vim.opt.smartindent = true
 
-o.smartindent = true
+vim.opt.wrap = false
 
-o.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-o.swapfile = false
-o.backup = false
-o.undodir = os.getenv("HOME") .. "/.vim/undodir"
-o.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
-o.hlsearch = false
-o.incsearch = true
+vim.opt.termguicolors = true
 
-o.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
-o.scrolloff = 8
-o.signcolumn = "yes"
-o.isfname:append("@-@")
+vim.opt.updatetime = 50
 
-o.updatetime = 50
-
-o.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
