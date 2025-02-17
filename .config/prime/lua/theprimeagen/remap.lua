ff269b20235133f1e9d86bbc334864269f5849c0
김찬희 <blackeagle10@icloud.com>
2025-02-17 10:24:01 +0900

    primeagen remap.lua to original

--- .config/prime/lua/theprimeagen/remap.lua
+++ .config/prime/lua/theprimeagen/remap.lua
@@ -1,61 +1,58 @@
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-local m = vim.keymap
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-m.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
-m.set("v", "J", ":m '>+1<CR>gv=gv")
-m.set("v", "K", ":m '<-2<CR>gv=gv")
-
-m.set("n", "J", "mzJ`z")
-m.set("n", "<C-d>", "<C-d>zz")
-m.set("n", "<C-u>", "<C-u>zz")
-m.set("n", "n", "nzzzv")
-m.set("n", "N", "Nzzzv")
-m.set("n", "<leader>zig", "<Cmd>LspRestart<CR>")
-
-m.set("n", "<leader>vwm", function()
+vim.keymap.set("n", "<leader>vwm", function()
 	require("vim-with-me").StartVimWithMe()
 end)
-m.set("n", "<leader>svwm", function()
+vim.keymap.set("n", "<leader>svwm", function()
 	require("vim-with-me").StopVimWithMe()
 end)
 
 -- greatest remap ever
-m.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-m.set({ "n", "v" }, "<leader>y", [["+y]])
-m.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-m.set({ "n", "v" }, "<leader>d", '"_d')
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 
 -- This is going to get me cancelled
-m.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
-m.set("n", "Q", "<nop>")
-m.set("n", "<C-f>", "<Cmd>silent !tmux neww tmux-sessionizer<CR>")
-m.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-m.set("n", "<C-k>", "<Cmd>cnext<CR>zz")
-m.set("n", "<C-j>", "<Cmd>cprev<CR>zz")
-m.set("n", "<leader>k", "<Cmd>lnext<CR>zz")
-m.set("n", "<leader>j", "<Cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-m.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-m.set("n", "<leader>x", "<Cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-m.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 
-m.set("n", "<leader>ea", 'oassert.NoError(err, "")<Esc>F";a')
+vim.keymap.set("n", "<leader>ea", 'oassert.NoError(err, "")<Esc>F";a')
 
-m.set("n", "<leader>ef", 'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj')
+vim.keymap.set("n", "<leader>ef", 'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj')
 
-m.set("n", "<leader>el", 'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i')
+vim.keymap.set("n", "<leader>el", 'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i')
 
-m.set("n", "<leader>vpp", "<Cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
-m.set("n", "<leader>mr", "<Cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
-m.set("n", "<leader><leader>", function()
+vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
