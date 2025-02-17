9a1adbf675d9bc9ccca990f20e01475f1d3d72e0
김찬희 <blackeagle10@icloud.com>
2025-02-17 10:23:49 +0900

    Update options.lua to do not comment line after comment

--- .config/nvim/lua/josean/core/options.lua
+++ .config/nvim/lua/josean/core/options.lua
@@ -1,67 +1,77 @@
+--[[ 
+  Netrw Configuration
+  Sets the file explorer (netrw) list style.
+]]
 vim.cmd('let g:netrw_liststyle = 3')
 
-local o = vim.opt -- for conciseness
+-- for conciseness
+local o = vim.opt
 
--- line numbers
-o.relativenumber = true -- show relative line numbers
-o.number = true -- shows absolute line number on cursor line (when relative number is on)
+o.relativenumber = true
+o.number = true
 
--- tabs & indentation
-local indent = 2
-
-o.tabstop = indent -- prettier default == 2
-o.shiftwidth = indent
-o.softtabstop = indent -- Tab, backspace behaves as much as indent width
-o.expandtab = true -- expand tab to spaces
-o.autoindent = true -- copy indent from current line when starting new one
-
--- line wrapping
-o.wrap = false
-
--- search settings
-o.ignorecase = true -- ignore case when searching
-o.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
--- cursor line
-o.cursorline = true -- highlight the current cursor line
-
--- appearance
-o.termguicolors = true
-o.background = 'dark' -- colorschemes that can be light or dark will be made dark
-o.signcolumn = 'yes' -- show sign column so that text doesn't shift
-
--- backspace
-o.backspace = 'indent,eol,start' -- allow backspace on indent, end of line or insert mode start position
-
--- clipboard
-o.clipboard:append('unnamedplus') -- use system clipboard as default register
-
--- split windows
-o.splitright = true -- split vertical window to the right
-o.splitbelow = true -- split horizontal window to the bottom
-
--- spelling
-o.spell = false
-o.spelllang = { 'en', 'cjk' } -- Do not mark Chinese, Japanese and Korean as error
-o.spellsuggest = 'best'
-
--- turn off swapfile
-o.swapfile = false
-
--- 'gg' goes to start of first line
+-- Ensure 'gg' moves the cursor to the start of the line.
 o.startofline = true
 
 o.scrolloff = 5
 
--- Highlight on Yank
+local indent = 2
+
+o.tabstop = indent -- A tab is displayed as 2 spaces
+o.shiftwidth = indent -- Indent by 2 spaces when auto-indenting
+o.softtabstop = indent -- Editing: Tab/Backspace behaves as 2 spaces
+o.expandtab = true -- Use spaces instead of literal tab characters
+o.autoindent = true -- Maintain indent of current line when starting a new one
+
+o.ignorecase = true
+o.smartcase = true
+
+o.termguicolors = true -- Enable true colors
+o.background = 'dark'
+o.cursorline = true
+o.signcolumn = 'yes' -- prevents text shifting
+o.wrap = false
+
+o.backspace = 'indent,eol,start' -- More intuitive backspacing
+
+o.clipboard:append('unnamedplus') -- Use the system clipboard
+
+-- Split window placement
+o.splitright = true
+o.splitbelow = true
+
+o.spell = false
+o.spelllang = { 'en', 'cjk' } -- Do not mark Chinese, Japanese, and Korean as errors
+o.spellsuggest = 'best' -- Use best suggestion for spelling corrections
+
+o.swapfile = false -- Disable swap file creation
+
+-- Performance and Timeout Settings
+o.ttyfast = true -- Optimize for fast terminals
+o.timeout = true -- Enable timeout for mapped sequences
+o.timeoutlen = 500 -- Wait 500ms for a mapping to complete
+o.ttimeoutlen = 100 -- Wait 100ms for key code sequences (helps with escape delays)
+o.lazyredraw = false -- Redraw screen immediately (can help with macros)
+
 vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+
+-- Highlight on Yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('HighlightYank', {}),
   pattern = '*',
   callback = function()
     vim.highlight.on_yank({
       higroup = 'IncSearch',
-      timeout = 200, -- Duration in milliseconds
+      timeout = 300, -- Highlight duration in milliseconds
     })
+  end,
+})
+
+-- Do not comment the line after comment
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove('r')
+    vim.opt_local.formatoptions:remove('o')
   end,
 })
